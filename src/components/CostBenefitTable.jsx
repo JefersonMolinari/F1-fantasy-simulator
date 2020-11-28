ff3201390980d2c8c7d611dc979371d6883bae1f
Jeferson Molinari <jeferson.bdc@hotmail.com>
2020-11-27 18:23:27 -0800

    refactor: refactor Filter Methods

--- src/components/CostBenefitTable.jsx
+++ src/components/CostBenefitTable.jsx
@@ -3,77 +3,40 @@ import TableHeader from './TableHeader';
 import TableBody from './CostBenefitTableBody';
 
 function CostBenefitTable(params) {
+    // objs.sort((a,b) => (a.last_nom > b.last_nom) ? 1 : ((b.last_nom > a.last_nom) ? -1 : 0)); 
+
     const [dataList, setDataList] = useState([...params.dataList]);
 
     function pointsPerMi() {
         setDataList((prevList) => {
-            const newList = prevList.sort(filterPointsPerMi);
+            const newList = prevList.sort((a, b) => ((a.points/a.price) > (b.points/b.price)) ? -1 : (((b.points/b.price) > (a.points/a.price)) ? 1 : 0));
             return [...newList];
         });
     }
     
     function price() {
         setDataList((prevList) => {
-            const newList = prevList.sort(filterPrice);
+            const newList = prevList.sort((a, b) => (a.price > b.price) ? -1 : ((b.price > a.price) ? 1 : 0));
             return [...newList];
         });
     }
     
     function points() {
         setDataList((prevList) => {
-            const newList = prevList.sort(filterPoints);
+            const newList = prevList.sort((a, b) => (a.points > b.points) ? -1 : ((b.points > a.points) ? 1 : 0));
             return [...newList];
         });
     }
     
     function name() {
         setDataList((prevList) => {
-            const newList = prevList.sort(filterName);
+            const newList = prevList.sort((a, b) => (a.name < b.name) ? -1 : ((b.name < a.name) ? 1 : 0));
             return [...newList];
         });
     }
-    
-    function filterPointsPerMi( a, b ) {
-        if ((a.points/a.price) > (b.points/b.price)){
-            return -1;
-        }
-        if ((b.points/b.price) > (a.points/a.price)){
-            return 1 ;
-        }
-        return 0;
-    }
-    
-    function filterPrice( a, b ) {
-        if (a.price > b.price){
-            return -1;
-        }
-        if (b.price > a.price){
-            return 1 ;
-        }
-        return 0;
-    }
-    
-    function filterPoints( a, b ) {
-        if (a.points > b.points){
-            return -1;
-        }
-        if (b.points > a.points){
-            return 1 ;
-        }
-        return 0;
-    }
-    
-    function filterName( a, b ) {
-        if (a.name > b.name){
-            return -1;
-        }
-        if (b.name > a.name){
-            return 1 ;
-        }
-        return 0;
-    }
+
     const tableHeader = [
-        { title: "POS", onClick: null },
+        { title: "POS"},
         { title: "Name", onClick: name },
         { title: "Points", onClick: points },
         { title: "Price", onClick: price },
