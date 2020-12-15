8e62c4f4547d69c741827b11451acd00424796d4
Jeferson Molinari <jeferson.bdc@hotmail.com>
2020-12-14 22:37:11 -0800

    refactor: refactored sorting table algorithm

--- client/src/components/CostBenefitTable.jsx
+++ client/src/components/CostBenefitTable.jsx
@@ -3,52 +3,41 @@ import TableHeader from './TableHeader';
 import TableBody from './CostBenefitTableBody';
 
 function CostBenefitTable(params) {
-    const [dataList, setDataList] = useState([...params.dataList]);
+    const [dataList, setDataList] = useState(params.dataList);
+    const [currentColumn, setCurrentColumn] = useState("");
 
-    function pointsPerMi() {
-        setDataList((prevList) => {
-            const newList = prevList.sort((a, b) => ((a.points/a.price) > (b.points/b.price)) ? -1 : (((b.points/b.price) > (a.points/a.price)) ? 1 : 0));
-            return [...newList];
-        });
-    }
-    
-    function price() {
-        setDataList((prevList) => {
-            const newList = prevList.sort((a, b) => (a.price > b.price) ? -1 : ((b.price > a.price) ? 1 : 0));
-            return [...newList];
-        });
-    }
-    
-    function points() {
-        setDataList((prevList) => {
-            const newList = prevList.sort((a, b) => (a.points > b.points) ? -1 : ((b.points > a.points) ? 1 : 0));
-            return [...newList];
-        });
-    }
-    
-    function name() {
-        setDataList((prevList) => {
-            const newList = prevList.sort((a, b) => (a.name < b.name) ? -1 : ((b.name < a.name) ? 1 : 0));
-            return [...newList];
-        });
-    }
+    function sortColumn(colName) {
+        let sortedList = [];
+
+        //if same column is clicked again, toogle sort order ASC/DESC
+        if (currentColumn === colName) {
+            sortedList = [...dataList].sort((a, b) => (a[colName] < b[colName]) ? -1 : ((b[colName] < a[colName]) ? 1 : 0));
+            setCurrentColumn("");
+        } else {
+            sortedList = [...dataList].sort((a, b) => (a[colName] > b[colName]) ? -1 : ((b[colName] > a[colName]) ? 1 : 0));
+            setCurrentColumn(colName);
+        }
+
+        setDataList(sortedList);
+    };
 
     const tableHeader = [
-        { title: "POS"},
-        { title: "Name", onClick: name },
-        { title: "Points", onClick: points },
-        { title: "Price", onClick: price },
-        { title: "Points/$mi", onClick: pointsPerMi }
-    ]
+        { id: "pos", title: "POS"},
+        { id: "name", title: "Name"},
+        { id: "points", title: "Points"},
+        { id: "price", title: "Price"},
+        { id: "pointsPerMi", title: "Points/$mi" }
+    ];
 
     return <table className="box">
         <TableHeader
             headers={tableHeader}
+            sortColumn={sortColumn}
         />
         <TableBody 
             dataList={dataList}
         />
     </table>
-}
+};
 
 export default CostBenefitTable;
--- client/src/components/TableHeader.jsx
+++ client/src/components/TableHeader.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 function TableHeader(params) {
-    const { headers } = params
+    const { headers, sortColumn } = params
     return <thead>
             <tr>
                 {headers.map((headItem, index) =>  (
-                    <th key={index} onClick={headItem.onClick}>{headItem.title}</th>
+                    <th key={index} onClick={() => {sortColumn(headItem.id)}}>{headItem.title}</th>
                 ))}
             </tr>
         </thead>
-}
+};
 
 export default TableHeader;
